3b1af80f236d381f0ffbe15c7e3ddc8273b13f7a
SameC137 <samemichael1415@gmail.com>
2021-06-21 20:26:09 +0300

    Remove Debug Code

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -60,37 +60,3 @@ class Clean_Tweets:
         df.drop(unwanted_rows ,inplace=True)
         
         return df
-
-
-if __name__ == "__main__":
-    # required column to be generated you should be creative and add more features
-    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
-    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("data/covid19.json")
-    tweet = TweetDfExtractor(tweet_list)
-    tweet_df = tweet.get_tweet_df() 
-    clean_tweets=Clean_Tweets(tweet_df)
-    # unwantedDropped=clean_tweets.drop_unwanted_column(tweet_df)
-    # print(unwantedDropped[unwantedDropped['retweet_count'] == 'retweet_count' ])
-
-    # dropped=clean_tweets.drop_duplicate(tweet_df)
-    # print(type(dropped))
-    # print(dropped[dropped.duplicated(original=False)])
-    
-
-    # clean=clean_tweets.remove_non_english_tweets(tweet_df)
-
-    # print(clean[clean['lang'] != 'en' ])
-
-    # data=clean_tweets.convert_to_numbers(tweet_df)
-
-    # print(data.dtypes)
-
-    # data=clean_tweets.convert_to_datetime(tweet_df)
-
-    # print(data.dtypes)
-
-        
-    # data=clean_tweets.drop_duplicate(tweet_df)
-
-    # print(data.duplicated(subset=["original_text"],keep="last").sum().astype(int))
